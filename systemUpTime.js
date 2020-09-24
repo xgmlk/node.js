bd66b211e9ca43473cadd2793d179fb828e7ebbc
xgmlk <ya.dimabl@gmail.com>
2020-09-24 17:17:55 +0400

    server

--- systemUpTime.js
+++ systemUpTime.js
@@ -8,7 +8,7 @@ function upTime() {
     let hour = Math.floor(sec / 60) / 60;
     let hourParse = Math.floor(hour * 10 / 10)
     let min = Math.floor((hour - hourParse) * 60) * 10 / 10
-    console.log(hourParse + ' Часов ' + min + ' Минут');
+    console.log(`${hourParse} Часов ${min} Минут `);
   }
 }
 
